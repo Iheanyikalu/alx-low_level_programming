718ee7aa37e3f13528702520a4085ffa490d58ce
Iheanyikalu <kaluiheanyi@gmail.com>
2023-08-17 09:10:32 -0700

    Update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,18 +2,18 @@
 /**
  * _isdigit - to check if paramaete is a digit or character
  *
- *  @c: parameter
- *
- *  return: 1 if its a number 0 otherwise
+ * @c: parameter*
+ * return: 1 if its a number 0 otherwise
  */
+
 int _isdigit(int ci)
 {
 	if (c >= 48 && c <= 57)
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
