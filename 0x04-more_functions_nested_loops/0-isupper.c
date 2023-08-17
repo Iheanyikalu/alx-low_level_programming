f238144d7c7f9c0623ca13854dad0dae881518ec
Iheanyikalu <kaluiheanyi@gmail.com>
2023-08-17 08:41:39 -0700

    add

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * int _isupper(int c); - detect upper case
+ * _isupper - detect upper case
  *
  * @c: input character
  *
  * Return: (1) for true (0) not true
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z' )
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
