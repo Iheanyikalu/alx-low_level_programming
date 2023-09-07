7d97eec8f3bcef6b1082a119aa41a006f48efe26
Iheanyikalu <kaluiheanyi@gmail.com>
2023-09-07 03:03:47 -0700

    all tasks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include "main.h"
 /**
  * reset_to98 - taking a pointer to an int
  *
  * @n: input
  * Return: 0
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
