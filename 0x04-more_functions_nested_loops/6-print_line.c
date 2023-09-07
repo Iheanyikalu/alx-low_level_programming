1e68ae836263fce08e8a22bfe7888d3290c8a792
Iheanyikalu <kaluiheanyi@gmail.com>
2023-09-07 00:26:49 -0700

    short distance

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,16 +4,20 @@
  * @n: input integer
  *
  */
-void print_line(int n);
+void print_line(int n)
 {
-	int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
 
-	for (i = 0; i < n; i++)
-	{
-	if (n > 0)
-	{
-	_putchar('_');
-	}
-	}
-	_putchar('\n');
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
