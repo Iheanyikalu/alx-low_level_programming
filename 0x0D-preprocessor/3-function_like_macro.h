298e7b01a39947883490cd46d0195ac3d2a6cebf
Iheanyikalu <kaluiheanyi@gmail.com>
2023-09-11 01:42:13 -0700

    All tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro
+ *
+ * @x: be input value
+ *
+ * Desc: header file
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
